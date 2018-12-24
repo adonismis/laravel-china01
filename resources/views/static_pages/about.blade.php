454c88a9d9b223d24623647651196693f51198f2
adonis <adonismis@gmail.com>
2018-12-24 16:38:15 +0800

    基本頁面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.layout_page')
+@section('title', '關於')
+@section('content')
+  <h1>關於頁</h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.layout_page')
+@section('title', '主要')
+@section('content')
+  <h1>主要頁</h1>
+@stop
